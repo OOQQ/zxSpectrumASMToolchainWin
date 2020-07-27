efaf344cd304fb6b8337ad5340669706116e9e27
OOQQ <ooqq@yahoo.com>
2020-07-27 12:45:32 +0100

    Update main.asm

--- src/main.asm
+++ src/main.asm
@@ -1,13 +1,13 @@
 		DEVICE ZXSPECTRUM48
-		org $8000								;code origin
+		org $8000					;code origin
 
 start:
-		ld hl, string						;address of string
+		ld hl, string					;address of string
 		call printString				;show string to the screen
 		ret
 
 newLine:
-		ld a, 13								;carriage return only - ZX doesn't like CHR(10)
+		ld a, 13					;carriage return only - ZX doesn't like CHR(10)
 		jr printChar
 
 printChar:
@@ -15,11 +15,11 @@ printChar:
 		push bc
 		push de
 		push af
-				ld a, 2							;select stream 2 = topscreen
-				call $1601					;ROM CHAN_OPEN
+			ld a, 2					;select stream 2 = topscreen
+			call $1601				;ROM CHAN_OPEN
 		pop af
 		push af
-				rst 16							;call &0010 print_a_1
+			rst 16					;call &0010 print_a_1
 		pop af
 		pop de
 		pop bc
@@ -27,7 +27,7 @@ printChar:
 		ret
 
 printString:
-		ld a, (hl)							;print a 0 terminated string
+		ld a, (hl)					;print a 0 terminated string
 		cp 0
 		ret z
 		inc hl
@@ -37,4 +37,4 @@ printString:
 string: db "Hello World!", 0
 
 		SAVETAP "../release/tape.tap", start
-														;save tape file, autolaunch start address is $8000
+		;save tape file, autolaunch start address is $8000
